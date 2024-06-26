c4142c597495c3220506d2eafec5738141316ad6
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-26 00:32:42 -0600

    feat: Add database connection functionality

---
+++ conn/conn.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://localhost:27017/ecommerce",
+      console.log("Database connected"),
+    );
+  } catch (error) {
+    console.log(error);
+    throw new Error("Database connection failed");
+  }
+};
+
+export default connectDB;
