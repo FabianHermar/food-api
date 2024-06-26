ede4231a50806ec50c05d22adfa056522d14ce4a
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-26 01:02:03 -0600

    chore: Update express app configuration and add default route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.urlencoded({ extended: true }))
+app.use( express.json() )
+app.use( morgan( 'dev' ) )
+
+app.get( '/', ( req, res ) => {
+  res.send( 'Hello World' )
+} )
+
+app.get( ( req, res ) => {
+  res.status( 404 ).json( { message: 'Not Found' } )
+} )
+
+app.listen( PORT, () => {
+  console.log( `Server is running on http://localhost:${PORT}` )
+} )
