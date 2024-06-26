7aebd165192f3343f49f1984a579aaf7c258b77e
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-26 01:22:01 -0600

    chore: Add CORS headers to allow cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import express from 'express'
 import morgan from 'morgan'
+import connectDB from '../conn/conn.js'
 
 const app = express()
 const PORT = 3000
 
+connectDB()
 app.use(express.urlencoded({ extended: true }))
 app.use( express.json() )
 app.use( morgan( 'dev' ) )
+
+app.use( ( req, res, next ) => {
+  res.setHeader( 'Access-Control-Allow-Origin', '*' );
+  res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE' );
+  res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type, Authorization' );
+  next();
+} );
 
 app.get( '/', ( req, res ) => {
   res.send( 'Hello World' )
