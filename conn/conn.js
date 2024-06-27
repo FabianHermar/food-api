772433885e5537af61ce4d282748c0b9cca5cab6
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-27 17:25:41 -0600

    chore: Update MongoDB connection URL to use environment variables and add error handling

--- conn/conn.js
+++ conn/conn.js
@@ -1,14 +1,19 @@
-import mongoose from "mongoose";
+import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb://localhost:27017/food",
-      console.log("Database connected"),
-    );
-  } catch (error) {
-    console.log(error);
-    throw new Error("Database connection failed");
+    const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_NAME}.zkvv1hu.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_NAME}`
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    await mongoose.connect( mongoURI, options );
+    console.log( 'Conectado a MongoDB' );
+  } catch ( error ) {
+    console.error( 'Error al conectar a MongoDB:', error );
+    process.exit( 1 ); // Detener el proceso si no se puede conectar a la base de datos
   }
 };
 
