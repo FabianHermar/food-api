7c9609f987f8f3b6ede383c98254f7cb00b0ec13
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-27 17:44:29 -0600

    chore: Update app.js to use environment variable for PORT

--- src/app.js
+++ src/app.js
@@ -9,24 +9,24 @@ const PORT = process.env.PORT || 3000
 connectDB()
 
 app.use(express.urlencoded({ extended: true }))
-app.use( express.json() )
-app.use( morgan( 'dev' ) )
+app.use(express.json())
+app.use(morgan('dev'))
 
-app.use( ( req, res, next ) => {
-  res.setHeader( 'Access-Control-Allow-Origin', '*' );
-  res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE' );
-  res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type, Authorization' );
-  next();
-} );
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 
-app.get( '/', ( req, res ) => {
-  res.send( 'Hello World' )
-} )
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
-app.get( ( req, res ) => {
-  res.status( 404 ).json( { message: 'Not Found' } )
-} )
+app.get((req, res) => {
+  res.status(404).json({ message: 'Not Found' })
+})
 
-app.listen( PORT, () => {
-  console.log( `Server is running on http://localhost:${PORT}` )
-} )
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
