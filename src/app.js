6cd9c7e3e634f38f9fc645ca6958a0b50c1a9f79
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-29 20:44:17 -0600

    refactor: Add FoodRoute to handle API endpoints for dishes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ import 'dotenv/config'
 import express from 'express'
 import morgan from 'morgan'
 import connectDB from './config/db.js'
+import FoodRoute from './routes/FoodRoute.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 connectDB()
 
+// Middlewares
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(morgan('dev'))
 
+// CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
@@ -20,11 +23,59 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.json( {
+    message: 'Welcome to the Food API',
+    food_api: 'API is running 🚀',
+    routes: [
+      {
+        method: 'GET',
+        get_dishes: '/api/dishes',
+      },
+      {
+        method: 'POST',
+        create_dish: '/api/dishes',
+        body: {
+          uuid: 'String',
+          name: 'String',
+          description: 'String',
+          ingredients: '[String]',
+          price: 'Number',
+          category: 'String'
+        }
+      },
+      {
+        method: 'GET',
+        get_dish_by_uuid: '/api/dishes/:uuid',
+      },
+      {
+        method: 'PUT',
+        update_dish_by_uuid: '/api/dishes/:uuid',
+        body: {
+          name: 'String',
+          description: 'String',
+          ingredients: '[String]',
+          price: 'Number',
+          category: 'String'
+        }
+      },
+      {
+        method: 'DELETE',
+        delete_dish_by_uuid: '/api/dishes/:uuid',
+      }
+    ],
+    note: 'This API is for educational purposes only. Do not use it in a production environment.',
+    additional_note: "Don't forget to check the README.md file for more information."
+  } )
+} )
 
-app.get((req, res) => {
-  res.status(404).json({ message: 'Not Found' })
+// Add the FoodRoute to the app
+app.use( '/api', FoodRoute )
+
+// 404 Route
+app.use( ( req, res ) => {
+  return res.status( 404 ).json( {
+    message: `Route ${req.url} Not found. 🙅‍♂️`
+  } )
 })
 
 app.listen(PORT, () => {
