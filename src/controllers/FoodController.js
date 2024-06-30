67bc52dd4fd2324a071ce66977e3acaeb201cebf
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-29 20:44:21 -0600

    feat: Add CRUD operations for dishes in FoodController

---
+++ src/controllers/FoodController.js
@@ -0,0 +1,72 @@
+import Dish from '../models/DishModel.js';
+
+// Get all dishes
+export const getDishes = async ( req, res ) => {
+  try {
+    const dishes = await Dish.find();
+    res.json( dishes );
+  } catch ( err ) {
+    res.status( 500 ).send( 'Server Error' );
+  }
+};
+
+// Create a dish
+export const createDish = async ( req, res ) => {
+  const { uuid, name, description, ingredients, price, category } = req.body;
+  try {
+    let dish = new Dish( { uuid, name, description, ingredients, price, category } );
+    await dish.save();
+    res.status( 201 ).json( dish );
+  } catch ( err ) {
+    res.status( 409 ).send( `Duplicate ID. The resource with ID ${uuid} already exists.` );
+  }
+};
+
+
+// Get a dish by uuid
+export const getDishByUuid = async ( req, res ) => {
+  const { uuid } = req.params;
+  try {
+    const dish = await Dish.findOne( { uuid } );
+    if ( !dish ) {
+      return res.status( 404 ).json( { msg: 'Dish not found' } );
+    }
+    res.json( dish );
+  } catch ( err ) {
+    res.status( 500 ).send( 'Server Error' );
+  }
+};
+
+
+// Update a dish by uuid
+export const updateDishByUuid = async ( req, res ) => {
+  const { uuid } = req.params;
+  const { name, description, ingredients, price, category } = req.body;
+  try {
+    let dish = await Dish.findOneAndUpdate(
+      { uuid },
+      { $set: { name, description, ingredients, price, category } },
+      { new: true }
+    );
+    if ( !dish ) {
+      return res.status( 404 ).json( { msg: 'Dish not found' } );
+    }
+    res.json( dish );
+  } catch ( err ) {
+    res.status( 500 ).send( 'Server Error' );
+  }
+};
+
+// Delete a dish by uuid
+export const deleteDishByUuid = async ( req, res ) => {
+  const { uuid } = req.params;
+  try {
+    let dish = await Dish.findOneAndRemove( { uuid } );
+    if ( !dish ) {
+      return res.status( 404 ).json( { msg: 'Dish not found' } );
+    }
+    res.json( { msg: 'Dish removed' } );
+  } catch ( err ) {
+    res.status( 500 ).send( 'Server Error' );
+  }
+};
