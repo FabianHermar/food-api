ea1ce351f9d8c4f3ceb01f3a1a8c3fd47d45c880
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-06-29 20:44:51 -0600

    feat: Add FoodRoute to handle API endpoints for dishes

---
+++ src/routes/FoodRoute.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createDish, deleteDishByUuid, getDishByUuid, getDishes, updateDishByUuid } from "../controllers/FoodController.js";
+
+const router = Router();
+
+router.get( '/dishes', getDishes );
+router.post( '/dishes', createDish );
+router.get( '/dishes/:uuid', getDishByUuid );
+router.put( '/dishes/:uuid', updateDishByUuid );
+router.delete( '/dishes/:uuid', deleteDishByUuid );
+
+export default router;
