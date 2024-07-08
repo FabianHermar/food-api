06dbcbc9f0264284c3fca90d0538f45214680ba6
MiguelMacias3M <miguelmacias3m@gmail.com>
2024-07-08 07:23:17 -0600

    Limpieza de codigo de la autenticacion

--- src/routes/AuthRoute.js
+++ src/routes/AuthRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import UserModel from '../models/UserModel.js'; // Importa el modelo de usuario
+import UserModel from '../models/UserModel.js'; 
 
 const router = express.Router();
 
--- src/routes/FoodRoute.js
+++ src/routes/FoodRoute.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { createDish, deleteDishByUuid, getDishByUuid, getDishes, updateDishByUuid } from '../controllers/FoodController.js';
-import authMiddleware from '../middleware/authMiddleware.js'; // Importa el middleware de autenticación
+import authMiddleware from '../middleware/authMiddleware.js'; 
 
 const router = Router();
 
-router.get('/dishes', getDishes);
-router.post('/dishes', authMiddleware, createDish); // Protege la ruta
+router.get('/dishes',authMiddleware, getDishes);
+router.post('/dishes', authMiddleware, createDish); 
 router.get('/dishes/:uuid', getDishByUuid);
-router.put('/dishes/:uuid', authMiddleware, updateDishByUuid); // Protege la ruta
-router.delete('/dishes/:uuid', authMiddleware, deleteDishByUuid); // Protege la ruta
+router.put('/dishes/:uuid', authMiddleware, updateDishByUuid); 
+router.delete('/dishes/:uuid', authMiddleware, deleteDishByUuid); 
 
 export default router;
