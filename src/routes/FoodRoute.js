d12274f45a62a1e7c1e4a76457386ed7115928f2
MiguelMacias3M <miguelmacias3m@gmail.com>
2024-07-08 07:31:15 -0600

    Se protegieron todas las rutas existentes

--- src/routes/FoodRoute.js
+++ src/routes/FoodRoute.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get('/dishes',authMiddleware, getDishes);
 router.post('/dishes', authMiddleware, createDish); 
-router.get('/dishes/:uuid', getDishByUuid);
+router.get('/dishes/:uuid', authMiddleware, getDishByUuid);
 router.put('/dishes/:uuid', authMiddleware, updateDishByUuid); 
 router.delete('/dishes/:uuid', authMiddleware, deleteDishByUuid); 
 
