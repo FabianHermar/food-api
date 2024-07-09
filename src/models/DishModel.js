3d93c96c6b5f3c453c01b63b4770758bfc43789a
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-07-09 13:39:41 -0600

    format: All files formated with the prettier config

--- src/models/DishModel.js
+++ src/models/DishModel.js
@@ -1,33 +1,42 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
-const DishSchema = new mongoose.Schema( {
-  uuid: {
-    type: String,
-    required: true,
-    unique: true
+const DishSchema = new mongoose.Schema(
+  {
+    uuid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    ingredients: {
+      type: [String],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        'Tacos',
+        'Enchiladas',
+        'Quesadillas',
+        'Sopas',
+        'Postres',
+        'Bebidas',
+      ],
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  ingredients: {
-    type: [ String ],
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  category: {
-    type: String,
-    enum: [ 'Tacos', 'Enchiladas', 'Quesadillas', 'Sopas', 'Postres', 'Bebidas' ],
-    required: true
-  }
-},
-  { versionKey: false } );
+  { versionKey: false }
+)
 
-export default mongoose.model( 'DishModel', DishSchema );
+export default mongoose.model('DishModel', DishSchema)
--- src/routes/FoodRoute.js
+++ src/routes/FoodRoute.js
@@ -1,12 +1,18 @@
-import { Router } from "express";
-import { createDish, deleteDishByUuid, getDishByUuid, getDishes, updateDishByUuid } from "../controllers/FoodController.js";
+import { Router } from 'express'
+import {
+  createDish,
+  deleteDishByUuid,
+  getDishByUuid,
+  getDishes,
+  updateDishByUuid,
+} from '../controllers/FoodController.js'
 
-const router = Router();
+const router = Router()
 
-router.get( '/dishes', getDishes );
-router.post( '/dishes', createDish );
-router.get( '/dishes/:uuid', getDishByUuid );
-router.put( '/dishes/:uuid', updateDishByUuid );
-router.delete( '/dishes/:uuid', deleteDishByUuid );
+router.get('/dishes', getDishes)
+router.post('/dishes', createDish)
+router.get('/dishes/:uuid', getDishByUuid)
+router.put('/dishes/:uuid', updateDishByUuid)
+router.delete('/dishes/:uuid', deleteDishByUuid)
 
-export default router;
+export default router
